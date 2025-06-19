efad8ef6985957ce1f5f6aac21df29a2ba6795ac
Hafied <darka433@gmail.com>
2025-06-19 15:34:54 +0700

    2nd commit "Setup Drizzle"

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,20 +1,26 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import drizzlePlugin from 'eslint-plugin-drizzle';
-import {fixupPluginRules} from '@eslint/compat';
+import drizzlePlugin from "eslint-plugin-drizzle";
+import { fixupPluginRules } from "@eslint/compat";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.browser },
+  },
   tseslint.configs.recommended,
   eslintConfigPrettier,
   {
-    plugins:{
-      drizzle:fixupPluginRules(drizzlePlugin)
-    }
-  }
+    plugins: {
+      drizzle: fixupPluginRules(drizzlePlugin),
+    },
+  },
 ]);
